0d144285c53f5d89c6027d4bd2381e957c108d25
hammednajimdeenadewale <186573953+hammednajimdeenadewale@users.noreply.github.com>
2024-11-21 04:53:02 -0800

    Add files via upload

---
+++ healthcare/healthcare/About.js
@@ -0,0 +1,16 @@
+document.getElementById('learnMore').addEventListener('click', () => {
+    alert('Learn more about our amazing services!');
+  });
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
